d49d1c3d2e5908962cabd3478a210248ec357cac
Warren White <warren1linux@gmail.com>
2017-11-14 12:09:04 +0530

    Update SubnetScript.sh

--- SubnetScript.sh
+++ SubnetScript.sh
@@ -29,33 +29,56 @@ function hostCalc {
 
    if [ $cidr -ge 24 ] && [ $cidr -le 30 ]         # CIDR values between 24 and 30
    	then
-   	 echo "The subnet mask is:";echo;
-   	 echo "255.255.255.$(( 256-$(( 2**exp )) )) "
-   	 echo
-   	 echo "The effective number of subnets is:"
-   	 echo
-   	 subnets=$(( 2**$((cidr%8)) ))
-   	 echo $subnets
+   	 echo "The subnet mask is:"
+       echo
+       if [ $cidr -eq 24 ]       # when CIDR equals 24
+         then
+             echo " 255.255.255.0 "  
+             echo "The effective number of subnets is:"
+             echo "1"
+       else        # otherwise
+   	       echo "255.255.255.$(( 256-$(( 2**exp )) )) "
+   	       echo
+   	       echo "The effective number of subnets is:"
+   	       echo
+   	       subnets=$(( 2**$((cidr%8)) ))
+   	       echo $subnets
+       fi
    elif [ $cidr -ge 16 ] && [ $cidr -lt 24  ]      # CIDR values between 16 and 24
    	then
    	 exp=$((exp%8))
-   	 echo "The subnet mask is";echo;
-   	 echo "255.255.$(( 256-$(( 2**exp )) )).0"
-   	 echo
-   	 echo "The effective number of subnets is:"
-   	 echo
-   	 subnets=$(( 2**$((cidr%8)) ))
-   	 echo $subnets
+   	 echo "The subnet mask is";
+       echo
+       if [ $cidr -eq 16 ]     # when CIDR equals 16
+          then
+             echo " 255.255.0.0 "  
+             echo "The effective number of subnets is:"
+             echo "1"
+       else            # otherwise
+   	       echo "255.255.$(( 256-$(( 2**exp )) )).0"
+   	       echo
+   	       echo "The effective number of subnets is:"
+   	       echo
+   	       subnets=$(( 2**$((cidr%8)) ))
+   	       echo $subnets
+       fi
    elif [ $cidr -ge 8 ] && [ $cidr -lt 16 ]        # CIDR values between 8 and 16
    	then
-   	 exp=$((exp%8))
-   	 echo "The subnet mask is";echo;
-   	 echo "255.$(( 256-$(( 2**exp )) )).0.0"
-   	 echo
-   	 echo "The effective number of subnets is:"
-   	 echo
-   	 subnets=$(( 2**$((cidr%8)) ))
-   	 echo $subnets;
+       if [ $cidr -eq 8 ]    # when CIDR equals 8
+          then
+             echo " 255.0.0.0 "  
+             echo "The effective number of subnets is:"
+             echo "1"
+       else         # otherwise
+   	       exp=$((exp%8)) 
+   	       echo "The subnet mask is";echo;
+   	       echo "255.$(( 256-$(( 2**exp )) )).0.0"
+   	       echo
+   	       echo "The effective number of subnets is:"
+   	       echo
+   	       subnets=$(( 2**$((cidr%8)) ))
+   	       echo $subnets;
+       fi
    fi
   echo
 }
